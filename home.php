d46e0151c274c13ee1e9e9cdd975306a7587e42d
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2024-08-12 10:16:25 +0300

    Print posts in different formats

--- home.php
+++ home.php
@@ -2,62 +2,96 @@
 
 <!--MAIN BANNER AREA START -->
 <div class="page-banner-area page-contact" id="page-banner">
-    <div class="overlay dark-overlay"></div>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-lg-8 m-auto text-center col-sm-12 col-md-12">
-                <div class="banner-content content-padding">
-                    <h1 class="text-white">Promodise журнал</h1>
-                    <p>Полезные статьи про маркетинг и диджитал</p>
-                </div>
-            </div>
+  <div class="overlay dark-overlay"></div>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-lg-8 m-auto text-center col-sm-12 col-md-12">
+        <div class="banner-content content-padding">
+          <h1 class="text-white">Promodise журнал</h1>
+          <p>Полезные статьи про маркетинг и диджитал</p>
         </div>
+      </div>
     </div>
+  </div>
 </div>
 <!--MAIN HEADER AREA END -->
 
 <section class="section blog-wrap ">
     <div class="container">
         <div class="row">
-            <div class="col-lg-8">
-            
-                    <div class="row">
-                      <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-                        <!-- Цикл WordPress -->
-                        <div class="col-lg-6">
-                            <div class="blog-post">
-                                <?php
-                                  if( has_post_thumbnail() ) {
-                                    the_post_thumbnail( 'medium', array('class' => "img-fluid"));
-                                  }
-                                  else {
-                                    echo '<img class="img-fluid" src="'.get_template_directory_uri().'/images/blog/blog-1.jpg" />';
-                                  }
-                                ?>
-                                <div class="mt-4 mb-3 d-flex">
-                                    <div class="post-author mr-3">
-                                        <i class="fa fa-user"></i>
-                                        <span class="h6 text-uppercase"><?php the_author(); ?></span>
-                                    </div>
+          <div class="col-lg-8">
+            <div class="row">
+              <?php 
+              $index = 0;
+              if ( have_posts() ) : while ( have_posts() ) : the_post();
+                $index++;
+                switch($index) {
+                  case "3": ?>
+                    <div class="col-lg-12">
+                      <div class="blog-post">
+                        <?php
+                          if( has_post_thumbnail() ) {
+                            the_post_thumbnail('post-thumbnail', array('class' => "img-fluid w-100"));
+                          }
+                          else {
+                            echo '<img class="img-fluid w-100" src="'.get_template_directory_uri().'/images/blog/blog-1.jpg" />';
+                          }
+                        ?>
+                        <div class="mt-4 mb-3 d-flex">
+                          <div class="post-author mr-3">
+                            <i class="fa fa-user"></i>
+                            <span class="h6 text-uppercase"><?php the_author(); ?></span>
+                          </div>
 
-                                    <div class="post-info">
-                                        <i class="fa fa-calendar-check"></i>
-                                        <span><?php the_time('j F Y'); ?></span>
-                                    </div>
-                                </div>
-                                <a href="<?php echo get_the_permalink(); ?>" class="h4 "><?php the_title(); ?></a>
-                                <p class="mt-3"><?php the_excerpt(); ?></p>
-                                <a href="<?php echo get_the_permalink(); ?>" class="read-more">Читать статью <i class="fa fa-angle-right"></i></a>
-                            </div>
+                          <div class="post-info">
+                            <i class="fa fa-calendar-check"></i>
+                            <span><?php the_time('j F Y'); ?></span>
+                          </div>
                         </div>
-                        
-                      <?php endwhile; else : ?>
-                        <p>Записей нет.</p>
-                      <?php endif; ?>
+                        <a href="<?php echo get_the_permalink(); ?>" class="h4 "><?php the_title(); ?></a>
+                        <p class="mt-3"><?php the_excerpt(); ?></p>
+                        <a href="<?php echo get_the_permalink(); ?>" class="read-more">
+                          Читать статью <i class="fa fa-angle-right"></i>
+                        </a>
+                      </div>
                     </div>
+                  <?php
+                    break;
+                    default: ?>
+                      <div class="col-lg-6">
+                        <div class="blog-post">
+                          <?php
+                            if( has_post_thumbnail() ) {
+                              the_post_thumbnail('post-thumbnail', array('class' => "img-fluid"));
+                            }
+                            else {
+                              echo '<img class="img-fluid" src="'.get_template_directory_uri().'/images/blog/blog-1.jpg" />';
+                            }
+                          ?>
+                          <div class="mt-4 mb-3 d-flex">
+                            <div class="post-author mr-3">
+                              <i class="fa fa-user"></i>
+                              <span class="h6 text-uppercase"><?php the_author(); ?></span>
+                            </div>
 
-                    
-
+                            <div class="post-info">
+                              <i class="fa fa-calendar-check"></i>
+                              <span><?php the_time('j F Y'); ?></span>
+                            </div>
+                          </div>
+                          <a href="<?php echo get_the_permalink(); ?>" class="h4 "><?php the_title(); ?></a>
+                          <p class="mt-3"><?php the_excerpt(); ?></p>
+                          <a href="<?php echo get_the_permalink(); ?>" class="read-more">
+                            Читать статью <i class="fa fa-angle-right"></i>
+                          </a>
+                        </div>
+                      </div>
+                  <?php
+                  break;
+                } endwhile; else : ?>
+                  <p>Записей нет.</p>
+                <?php endif; ?>
+              </div>
             </div>
             <div class="col-lg-4">
                       <div class="row">
